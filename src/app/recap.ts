9a67949e445c6e108bcb9b698b07265a788b20dc
rafael31p <rafael31p@gmail.com>
2023-06-10 11:54:56 -0500

    se guarda de nuevo

---
+++ src/app/recap.ts
@@ -0,0 +1,15 @@
+const username: string = 'rafael31p';
+const suma = (a: number, b: number) =>{
+  return a+b;
+}
+
+suma(1,2);
+
+class Person{
+
+  constructor(private age: number, private lasName: string){
+  }
+}
+
+const nico = new Person(26, 'Peña');
+
