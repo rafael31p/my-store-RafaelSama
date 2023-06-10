1404463ec67f56f45d300c08e463de414e751165
rafael31p <rafael31p@gmail.com>
2023-06-10 11:53:05 -0500

    se generan cambios para el front de la tienda virtual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'my-store-RafaelSama';
+  public name = 'RafaelSama';
+  public age = 26;
+  public img = 'https://source.unsplash.com/random';
+  public btnDisabled = true;
+
+  person = {
+    name: 'RafaelSama',
+    age: 26,
+    avatar: 'https://source.unsplash.com/random'
+  }
+
+  toggleButton () {
+    this.btnDisabled = !this.btnDisabled;
+  }
+  increaseAge() {
+    this.person.age+=1;
+  }
 }
